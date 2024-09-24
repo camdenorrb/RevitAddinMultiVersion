d481e861e4f6eb0416f2e7cace17bed1f3672fd8
camdenorrb <dev@12oclock.dev>
2024-09-24 00:05:34 -0500

    Testing

--- RevitAddinMultiVersion/Program.cs
+++ RevitAddinMultiVersion/Program.cs
@@ -6,6 +6,7 @@ class App : IExternalApplication
 {
     public Result OnStartup(UIControlledApplication application)
     {
+        TaskDialog.Show("Remember, I'm always watching >:3", "MultiVersion:" + getVersion());
         return Result.Succeeded;
     }
 
@@ -13,4 +14,37 @@ class App : IExternalApplication
     {
         return Result.Succeeded;
     }
+
+
+    public string getVersion()
+    {
+        #if REVIT2014
+                return "R14";
+        #elif REVIT2015
+                return "R15";
+        #elif REVIT2016
+                return "R16";
+        #elif REVIT2017
+                return "R17";
+        #elif REVIT2018
+                return "R18";
+        #elif REVIT2019
+                return "R19";
+        #elif REVIT2020
+                return "R20";
+        #elif REVIT2021
+                return "R21";
+        #elif REVIT2022
+                return "R22";
+        #elif REVIT2023
+                return "R23";
+        #elif REVIT2024
+                return "R24";
+        #elif REVIT2025
+                return "R24";
+        #else
+                return "Unknown version";
+        #endif
+    }
+
 }
